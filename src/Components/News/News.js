864be921ba2969cc3fe43fdabd031bb15e59ce5c
rokydas <droky136@gmail.com>
2020-08-29 19:25:15 +0600

    simple button and card added

---
+++ src/Components/News/News.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button, makeStyles } from '@material-ui/core';
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+  });
+
+const News = () => {
+    const classes = useStyles();
+    return (
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="140"
+                    image=""
+                    title="Contemplative Reptile"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Lizard
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                        across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Share
+                </Button>
+                <Button size="small" color="primary">
+                    Learn More
+                </Button>
+            </CardActions>
+        </Card>
+    );
+};
+
+export default News;
