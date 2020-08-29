20fe9f4f9bc4aa290b8d55415a858af1849eb042
rokydas <droky136@gmail.com>
2020-08-29 20:15:00 +0600

    newspaper done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from '@material-ui/core';
 import './App.css';
 import News from './Components/News/News';
 
 function App() {
+  const [ articles, setArticles ] = useState([]);
+  useEffect( () => {
+    fetch('http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=619e767a2687413da2955c03efba943d')
+    .then(res => res.json())
+    .then(data => setArticles(data.articles));
+  }, []);
   return (
     <div>
       <Button color="primary">Hello World</Button>
@@ -13,7 +19,10 @@ function App() {
       <Button variant="contained" color="secondary">
         Secondary
       </Button>
-      <News></News>
+      <h2>Headlines: {articles.length}</h2>
+      {
+        articles.map(article => <News key={article.description} data={article}></News>)
+      }
     </div>
   );
 }
--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -6,7 +6,10 @@ const useStyles = makeStyles({
     },
   });
 
-const News = () => {
+const News = (props) => {
+    const data = props.data; 
+    const { urlToImage, title, description } = data;
+    // console.log(props.data);
     const classes = useStyles();
     return (
         <Card className={classes.root}>
@@ -15,16 +18,15 @@ const News = () => {
                     component="img"
                     alt="Contemplative Reptile"
                     height="140"
-                    image=""
+                    image={urlToImage}
                     title="Contemplative Reptile"
                 />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
-                        Lizard
+                        {title}
                     </Typography>
                     <Typography variant="body2" color="textSecondary" component="p">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica
+                        {description}
                     </Typography>
                 </CardContent>
             </CardActionArea>
